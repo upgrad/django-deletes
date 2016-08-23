ce132a28745cd1af4d3d26a10873bf898659f314
Ankit <ankitml@gmail.com>
2016-08-23 15:38:09 +0530

    adding packaging information

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-deletes',
+    version='0.01',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A fully functional soft deletes (logical deletes) for django.',
+    long_description=README,
+    url='https://github.com/upgrad/django-deletes',
+    author='Ankit Mittal',
+    author_email='ankit@upgrad.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
