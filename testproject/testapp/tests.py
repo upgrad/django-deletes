0e625e626c12ec62fa18aab6ee154f46cac6a499
Ankit <ankitml@gmail.com>
2016-12-05 17:02:52 +0530

    tests rolling out

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django_dynamic_fixture import G
+from testapp.models import Animal, Jungle, AnimalInJungle
 
-# Create your tests here.
+
+class TestModel(TestCase):
+
+    def test_delete(self):
+        animal = G(Animal)
+        jungle = G(Jungle, apex_predator=animal)
+        animal_id = animal.id
+        jungle_id = jungle.id
+        animal.delete()
+
+        deleted_animal = Animal.objects.filter(pk=animal_id).first()
+        deleted_jungle = Animal.objects.filter(pk=jungle_id).first()
+        self.assertTrue(deleted_animal.deleted)
+        self.assertTrue(deleted_jungle.deleted)
+
+        self.assertRaises(Exception, Animal.objects.get(pk=animal_id))
+        self.assertRaises(Exception, Jungle.objects.get(pk=jungle_id))
+
+    def test_final_delete(self):
+        animal = G(Animal)
+        jungle = G(Jungle, apex_predator=animal)
+        animal_id = animal.id
+        jungle_id = jungle.id
+        animal.delete(final=True)
+
+        self.assertEqual(Animal.objects.count(), 0)
+        self.assertEqual(Jungle.objects.count(), 0)
